10f7d0cc1aa2b31071dd18d4aafedeb599c62e00
Renato <102629460+cibero42@users.noreply.github.com>
2025-04-30 19:34:21 +0200

    fix

--- build/entrypoint.sh
+++ build/entrypoint.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 
 if [ "$ENABLE_CRON" = "true" ]; then
+    echo "1st run"
+    /download.sh
     echo "Cron is enabled. Installing crontab and starting crond..."
     crontab /crontab
     crond -f -L /var/log/cron.log
 else
     echo "Cron is disabled. Running entrypoint.sh directly..."
-    /entrypoint.sh
+    /download.sh
 fi
